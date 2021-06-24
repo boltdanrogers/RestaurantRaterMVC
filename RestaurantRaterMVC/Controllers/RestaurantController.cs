fa36f4ced87c8cf2e277d79bf04b7316bbc5be9f
Daniel Rogers <boldanrogers@gmail.com>
2021-06-24 18:40:25 -0400

    Added the rest of the create chtml, along with a couple methods in the restaurant controller

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -17,6 +17,32 @@ namespace RestaurantRaterMVC.Controllers
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
-        }
-    }
+        }//end of method Index
+
+        //Get: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }//end of method Create
+
+
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //only get here is modelState is invalid
+            return View(restaurant);
+
+
+        }//end of method Create
+
+
+    }//end of class RestaurantController
 }
